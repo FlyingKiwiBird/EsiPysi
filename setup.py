b3c75095f9353d780fdf9e2b28e9ca0c8d4aa321
Flying Kiwi <kiwi@flyingkiwibird.com>
2018-02-26 11:46:58 -0500

    Added setup info

--- setup.py
+++ setup.py
@@ -1,2 +1,35 @@
-import os
+import io
 from setuptools import setup
+
+install_requirements = [
+    "requests"
+]
+
+with io.open('readme.md') as reader:
+    readme = reader.read()
+
+
+
+setup(
+    name = "esipysi",
+    version = "0.1",
+    author = "Flying Kiwi",
+    author_email = "github@flyingkiwibird.com",
+    description = ("A client for ESI, the API for Eve Online"),
+    license = "MIT",
+    keywords = "Esi Eve Python Api",
+    url = "https://github.com/FlyingKiwiBird/EsiPysi",
+    packages=['esipysi'],
+    install_requires=install_requirements,
+    long_description=readme,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -6,7 +6,7 @@ import datetime
 class BasicTests(unittest.TestCase):
 
     def test_simple_op(self):
-        esi = EsiPysi("https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility", user_agent="Eve Test")
+        esi = EsiPysi("https://esi.tech.ccp.is/_latest/swagger.json?datasource=tranquility", user_agent="Eve Test")
         op = esi.get_operation("get_search")
         result = op.execute({"categories" : "character", "search" : "Flying Kiwi Sertan"})
         self.assertEqual(result, {'character': [95095106]})
